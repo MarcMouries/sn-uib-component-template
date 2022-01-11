7d7a84bb8d690c88fa55d16d79951ad46c40a3e1
MarcMouries <marc@mouries.net>
2022-01-11 18:01:59 -0500

    successful build

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # now-component-template
 
+Template to create a Now Experience Component
+
+## Installation 
+
+1. clone repository
+2. npm install
+3. snc ui-component develop --open
---
+++ example/element.js
@@ -0,0 +1,5 @@
+import '../src/x-snc-mycomponent';
+const el = document.createElement('DIV');
+document.body.appendChild(el);
+
+el.innerHTML = `<x-snc-mycomponent></x-snc-mycomponent>`;
