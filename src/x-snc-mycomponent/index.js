348c6a49852b5155374ec91a5afd2bbd8dfd5728
MarcMouries <marc@mouries.net>
2022-01-13 06:49:36 -0500

    moved properties out

--- src/x-snc-mycomponent/index.js
+++ src/x-snc-mycomponent/index.js
@@ -2,14 +2,12 @@ import { createCustomElement } from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 import view from './view';
+import properties from './properties'
 
-import {TASK_LIST} from './sample_data';
 
 createCustomElement('x-snc-mycomponent', {
-	renderer: { type: snabbdom, view },
+	renderer: { type: snabbdom },
 	view,
 	styles,
-	properties: {
-		data: {default: TASK_LIST}
-	}
+	properties
  });
