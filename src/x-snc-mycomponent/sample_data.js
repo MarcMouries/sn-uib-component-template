86e19e6ac01847c08c5011cc0d6405ae501f13cd
MarcMouries <marc@mouries.net>
2022-01-14 20:31:25 -0500

    + removing extraneous spaces

--- src/x-snc-mycomponent/sample_data.js
+++ src/x-snc-mycomponent/sample_data.js
@@ -1,5 +1,5 @@
 export const TASK_LIST = [
-    { "id": 0, "description": 'ToDo' },
-    { "id": 1, "description": 'Work in Progress'},
-    { "id": 2, "description": 'Done' }
+{"id": 0, "description": 'ToDo' },
+{"id": 1, "description": 'Work in Progress'},
+{"id": 2, "description": 'Done' }
 ];
